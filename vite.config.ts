92eade92be82c1ff06d1b738ca2ac104b2e1beb6
jyun <jyun-rnd@skvip.org>
2024-05-26 11:36:09 +0800

    chore: modify build dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": "/src", // 將 @ 映射到 src
+    },
+  },
+  build: {
+    // 改變輸出資料的檔案
+    outDir: "docs",
+  },
+});
